b399ca1b4efad9d7eda3d2ec41c9f5226afe7579
Deepanshu Justa <deepanshu.justa@netgen.in>
2025-02-18 10:50:40 +0530

    Basic Auth Setup - Login/Logout with middleware

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,36 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-Route::get('/', function () {
-    return view('layouts.admin');
+Route::redirect('/', '/login');
+
+/**
+ * ---------------------------
+ * Authentication Routes
+ * ---------------------------
+ */
+Route::view('/login', 'auth.login')->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'postLogin'])->name('login.post')->middleware('guest');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+
+/**
+ * ---------------------------
+ * Admin Routes
+ * ---------------------------
+ */
+Route::middleware('auth')->group(function() {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
 });
 
-Route::post('/logout', function() {
 
-})->name('logout');
-Route::post('/dashboard', function() {
-
-})->name('dashboard');
-
+/**
+ * ------------------------------
+ * Test Routes
+ * ------------------------------
+ */
 Route::controller(TestController::class)->prefix('/test')->group(function() {
     Route::view('page','test.page');
 });
